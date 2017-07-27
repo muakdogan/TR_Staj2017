48eb45c1d0f7b4a8443827955162fc0835743499
Yago <yagomus@windowslive.com>
2017-07-27 14:24:36 +0300

    kullanıcı guest middleware (RedirectIfAuthenticated) kaldırıldı, admin/login için login durum kontrolü eklendi

--- app/Http/Controllers/AdminAuth/AuthController.php
+++ app/Http/Controllers/AdminAuth/AuthController.php
@@ -38,11 +38,16 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+        //$this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+        //Login halinde iken tekrar admin/login'e girildiğinde /ilanAra'ya yönlendirdiği için yoruma alındı
     }
 
     public function showLoginForm(){
 
+        if (\Auth::guard('admin')->check())//Admin zaten giriş yaptıysa login ekranını gösterme
+        {
+            return redirect('admin/dashboard');
+        }
         if(view()->exists('auth.authenticate')){
             return view('auth.authenticate');
         }
