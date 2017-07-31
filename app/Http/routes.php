0ec26c5afd0fae534deac5ca6c9403b343d7beef
Yago <yagomus@windowslive.com>
2017-07-31 17:30:22 +0300

    merge sırasında atlanan kısımlar düzeltildi

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -65,7 +65,7 @@ Route::get('/DbTest', function () {
 
   //public/admin/login
   //public in altındaki klasörün içine yönlendiriyor
-  Route::get('/admi','AdminAuth\AuthController@showLoginForm');
+  Route::get('/admin/login','AdminAuth\AuthController@showLoginForm');
 
 
   Route::post('/admin/login','AdminAuth\AuthController@login');
@@ -146,10 +146,10 @@ Route::get('/findChildrenTree/{sektor_id}', function ($sektor_id) {
     return view('admin.genproduction.projects');
   }]);
 
-  Route::get('/api/v1/admins/{id?}', 'Admins@index');
-  Route::post('/api/v1/admins', 'Admins@store');
-  Route::post('/api/v1/admins/{id}', 'Admins@update');
-  Route::delete('/api/v1/admins/{id}', 'Admins@destroy');
+  Route::get('/admin/api/v1/admins/{id?}', 'Admins@index');
+  Route::post('/admin/api/v1/admins', 'Admins@store');
+  Route::post('/admin/api/v1/admins/{id}', 'Admins@update');
+  Route::delete('/admin/api/v1/admins/{id}', 'Admins@destroy');
 
   /*Route::get('/adminAnasayfa', function () {
   $firmalar=Firma::all();
