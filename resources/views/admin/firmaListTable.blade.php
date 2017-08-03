93cf641c2b8d5b9c15f37282a9d83e25d518671f
Yago <yagomus@windowslive.com>
2017-08-03 14:17:38 +0300

    firma onay formlarının validasyonları eklendi

--- resources/views/admin/firmaListTable.blade.php
+++ resources/views/admin/firmaListTable.blade.php
@@ -7,52 +7,53 @@
       <th>ONAY</th>
     </tr>
   @foreach($onay as $firma)
+    {{-- buradaki hücreler, firma onaylama formuna dahil --}}
+
     <tr class="firmaSatir">
-      {{-- buradaki hücreler, firma onaylama formuna dahil --}}
 
-      <form method="POST" action="{{route('firmaOnaySubmit')}}">
+      <form method="POST" action="{{route('firmaOnaySubmit')}}" class="firmaOnayForm">
 
-      <input type="hidden" name="firma_id" value="{{$firma->id}}">
+        <input type="hidden" name="firma_id" value="{{$firma->id}}">
 
-      <td>{{$firma->olusturmaTarihi}} </td>
-      <td>{{$firma->adi}}</td>
-      <td>
-        <select class="onayTuruSecim" name="onay_turu">
-          <option value="0">Standart</option>
-          <option value="1">Ödemesiz</option>
-          <option value="2">Özel</option>
-          <option value="3">Ret</option>
-        </select>
-      </td>
-      <td>
-        <div class="metot">
-          
-        </div>
-        <div class="metot" style="display: none;">
-          <label class="col-md-4">Üyelik süresi (ay): </label>
-          <input class="col-md-4" type="number" name="uyelik_bitis_suresi">
-        </div>
-        <div class="metot" style="display: none;">
-          <div class="form-group">
-            <label class="col-md-4">Miktar: </label>
-            <input class="col-md-4" type="number" name="miktar"><br>
+        <td>{{$firma->olusturmaTarihi}} </td>
+        <td>{{$firma->adi}}</td>
+        <td>
+          <select class="onayTuruSecim" name="onay_turu">
+            <option value="0">Standart</option>
+            <option value="1">Ödemesiz</option>
+            <option value="2">Özel</option>
+            <option value="3">Ret</option>
+          </select>
+        </td>
+        <td>
+          <div class="metot">
+            
           </div>
-
-          <div class="form-group">
+          <div class="metot" style="display: none;">
             <label class="col-md-4">Üyelik süresi (ay): </label>
-            <input class="col-md-4" type="number" name="sure"><br>
+            <input class="col-md-4 uyelikBitisSuresi" type="number" name="uyelik_bitis_suresi" pattern="[0-9]*" min="1" max="12" title="1 ila 12 ay">
           </div>
+          <div class="metot" style="display: none;">
+            <div class="form-group">
+              <label class="col-md-4">Miktar: </label>
+              <input class="col-md-4 miktar" name="miktar" pattern="[0-9]*" title="Pozitif sayı"><br>
+            </div>
 
-          <div class="form-group">
-            <label class="col-md-4">Teklif geçerlilik süresi (ay): </label>
-            <input class="col-md-4" type="number" name="gecerlilik_sure"><br>
+            <div class="form-group">
+              <label class="col-md-4">Üyelik süresi (ay): </label>
+              <input class="col-md-4 sure" type="number" name="sure" pattern="[0-9]*" min="1" max="12" title="1 ila 12 ay"><br>
+            </div>
+
+            <div class="form-group">
+              <label class="col-md-4">Teklif geçerlilik süresi (ay): </label>
+              <input class="col-md-4 gecerlilikSure" type="number" name="gecerlilik_sure" pattern="[0-9]*" min="1" max="12" title="1 ila 12 ay"><br>
+            </div>
           </div>
-        </div>
-        <div class="metot" style="display: none;">
-          
-        </div>
-      </td>
-      <td><input style="float:left" id="{{$firma->id}}" type="submit" class="btn btn-primary" value="ONAYLA" onclick="alert('FİRMA ONAYLANDI');"></td>
+          <div class="metot" style="display: none;">
+            
+          </div>
+        </td>
+        <td><input style="float:left" id="{{$firma->id}}" type="submit" class="btn btn-primary onayButon" value="ONAYLA"></td>
 
       </form>
     </tr>
@@ -80,6 +81,43 @@
 
     $('.onayTuruSecim').change(function(){
       metotGoster($(this).parents('.firmaSatir'), $(this).prop('selectedIndex'));
+    });
+
+    $('.onayButon').click(function(){
+      $("input[type=submit]", $(this).parents(".firmaSatir")).removeAttr("clicked");
+      $(this).attr("clicked", "true");
+    });
+
+    //ONAYLA tuşuna basıldığında formun geçerli olup olmadığına bak
+    $(".firmaOnayForm").submit(function(event){
+
+      //Tuhaflık: jQuery, form inputlarını <form>'un değil <tr class="firmaSatir">'ın çocuğu olarak görüyor
+
+      var satir = $(this).parents(".firmaSatir");
+      var onayTuru = $(satir).find('.onayTuruSecim').prop('selectedIndex');
+
+      if (onayTuru == 1)//ödemesiz onaya gereken field doldurulmuş mu?
+      {
+        if ($(satir).find('.uyelikBitisSuresi').val().length == 0)
+        {
+          console.log("Hata: Ödemesiz onay alanı boş.");
+          event.preventDefault();
+          return false;
+        }
+        return true;
+      }
+
+      if (onayTuru == 2)//özel onaya gereken field'lar doldurulmuş mu?
+      {
+        if ($(satir).find('.miktar').val().length == 0
+          || $(satir).find('.sure').val().length == 0
+          || $(satir).find('.gecerlilikSure').val().length == 0)
+        {
+          console.log("Hata: Özel onay alanları boş.");
+          event.preventDefault();
+          return false;
+        }
+      }
     });
   </script>
 
