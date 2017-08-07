48ccaebc4b8eb905dd8cd7bc965bc32818a77a55
Yago <yagomus@windowslive.com>
2017-08-07 11:18:08 +0300

    teklif gönderme ilkesi eklendi

--- app/Policies/IlanPolicy.php
+++ app/Policies/IlanPolicy.php
@@ -30,4 +30,14 @@ class IlanPolicy
         return $rol_id !== '3'//bu değer db'den string olarak geliyor
             && $yetkiliKullanici;//URL'ye başka firmanın id'si yazarak işlem yapılmaması için
     }
+    public function teklifGonder(Kullanici $user, $ilan, $firma_id)
+    {
+        $rol_id = $user->get_role_id($firma_id);
+
+        //kullanıcıyı session'daki firma ile eşleştir. Alakasız kullanıcı ise null dönecek
+        $yetkiliKullanici = DB::table('firma_kullanicilar')->where('kullanici_id', '=', $user->id)->where('firma_id', '=', $firma_id)->get();
+
+        return $rol_id !== '2'//bu değer db'den string olarak geliyor
+            && $yetkiliKullanici;//URL'ye başka firmanın id'si yazarak işlem yapılmaması için
+    }
 }
