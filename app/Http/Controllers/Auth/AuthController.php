12270fdf39c5dac75c07e92644ba2e758971c6da
Yago <yagomus@windowslive.com>
2017-08-08 13:00:56 +0300

    firma kayıt formunu işleyen fonksiyon, formun yeni hali için güncellendi (fatura bilgileri)

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -151,10 +151,49 @@ class AuthController extends Controller
             $adres->il_id = $request->il_id;
             $adres->ilce_id = $request->ilce_id;
             $adres->semt_id = $request->semt_id;
-            $adres->adres =Str::title(strtolower( $request->adres));
+            $adres->adres =Str::title(strtolower( $request->firma_adres));
             $tur = 1;
             $adres->tur_id = $tur;
             $firma->adresler()->save($adres);
+
+            if ($request->adres_kopyalayici == null)
+            {
+                $fatura_adres = new \App\Adres();
+                $fatura_adres->il_id = $request->fatura_il_id;
+                $fatura_adres->ilce_id = $request->fatura_ilce_id;
+                $fatura_adres->semt_id = $request->fatura_semt_id;
+                $fatura_adres->adres = Str::title(strtolower( $request->fatura_adres));
+                $fatura_adres->tur_id = 2;
+                $firma->adresler()->save($fatura_adres);
+            }
+            else if ($request->adres_kopyalayici == "kopyala")
+            {
+                $fatura_adres = new \App\Adres();
+                $fatura_adres->il_id = $request->il_id;
+                $fatura_adres->ilce_id = $request->ilce_id;
+                $fatura_adres->semt_id = $request->semt_id;
+                $fatura_adres->adres = Str::title(strtolower( $request->firma_adres));
+                $fatura_adres->tur_id = 2;
+                $firma->adresler()->save($fatura_adres);
+            }
+
+            $mali = new \App\MaliBilgi();
+
+            if ($request->fatura_tur == "kurumsal")
+            {
+                $mali->unvani = $request->firma_unvan;
+                $mali->vergi_numarasi = $request->vergi_no;
+            }
+
+            else if ($request->fatura_tur == "bireysel")
+            {
+                $mali->unvani = $request->ad_soyad;
+                $mali->vergi_numarasi = $request->tc_kimlik;                
+            }
+
+            $mali->vergi_dairesi_id = $request->vergi_daire;
+
+            $firma->mali_bilgiler()->save($mali);
 
             $firma->sektorler()->attach($request->sektor_id);
 
