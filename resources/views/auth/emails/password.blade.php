47177b01e877bbc984385418bc8e3c54fe7df3ff
Yago <yagomus@windowslive.com>
2017-08-16 14:25:49 +0300

    Şifre değiştirme email'i kullanıcı türüne göre dinamikleştirildi

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,17 @@
-Şifrenizi sıfırlamak için tıklayın: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+<?php
+	if ($user->password == "")
+	{
+		echo "Şifrenizi belirlemek için tıklayın: ";
+		$belirleme = 1;
+	}
+	
+	else
+	{
+		echo "Şifrenizi sıfırlamak için tıklayın: ";
+		$belirleme = 0;
+	}
+?>
+
+token: {{$token}}<br>
+
+<a href="{{ $link = url('password/reset', [$belirleme, $token]).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
