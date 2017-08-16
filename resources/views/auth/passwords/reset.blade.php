9948e2db1d1481046df33345ebab14c12ea1d17d
Yago <yagomus@windowslive.com>
2017-08-16 14:26:16 +0300

    Yeni şifre belirleme view'ı kullanıcı türüne göre dinamikleştirildi

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -9,7 +9,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Şifre Yenile</div>
+                <div class="panel-heading">{{$butonString}}</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
@@ -65,7 +65,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-refresh"></i>Şifre Yenile
+                                    <i class="fa fa-btn fa-refresh"></i>{{$butonString}}
                                 </button>
                             </div>
                         </div>
